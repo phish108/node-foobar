4c4d16a2e3afdc4678e2049e6eb6337508117961
Christian P. Glahn <cpglahn@gmail.com>
2020-02-28 07:59:57 +0100

    add initial code

---
+++ index.js
@@ -0,0 +1,4 @@
+
+module.exports  = function hello() {
+    return "hello";
+};
---
+++ test/01-init.js
@@ -0,0 +1,19 @@
+/* eslint-env node, mocha */
+/* eslint-disable require-jsdoc */
+
+const chai = require("chai");
+const expect = chai.expect;
+
+const greeting = require("../index.js");
+
+describe( "sanitize tool names", function() {
+    it("getting started", () => {
+        result = greeting();
+        expect(result).to.equal("hello world");
+    });
+
+    it("greet phish", () => {
+        result = greeting("phish");
+        expect(result).to.equal("hello phish");
+    })
+});
