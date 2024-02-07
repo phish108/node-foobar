eeedc8a7fcc8de12c4b84b4b5d94509f22f368f0
Christian Glahn <glah@zhaw.ch>
2024-02-07 10:17:11 +0100

    hello dates

--- index.js
+++ index.js
@@ -2,3 +2,10 @@
 module.exports  = function hello(target = "world") {
     return `hello ${target}`;
 };
+
+function calculateDayBetweenDates(date1, date2) {
+    const diffTime = Math.abs(date2 - date1);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    return diffDays;
+}
+
--- test/01-init.js
+++ test/01-init.js
@@ -1,19 +1,23 @@
 /* eslint-env node, mocha */
 /* eslint-disable require-jsdoc */
 
-const chai = require("chai");
-const expect = chai.expect;
+import { expect } from 'chai';
+import { init } from '../src/index';
 
-const greeting = require("../index.js");
+describe('init', () => {
+  it('should return 0', () => {
+    expect(init()).to.equal(0);
+  });
 
-describe( "sanitize tool names", function() {
-    it("getting started", () => {
-        result = greeting();
-        expect(result).to.equal("hello world");
-    });
+  it("calculateDayBetweenDates should return 0", () => {
+    const date1 = new Date("2020-01-01");
+    const date2 = new Date("2020-01-01");
+    expect(calculateDayBetweenDates(date1, date2)).to.equal(0);
+  });
 
-    // it("greet phish", () => {
-    //     result = greeting("phish");
-    //     expect(result).to.equal("hello phish");
-    // })
+  it("calculateDayBetweenDates should return 1", () => {
+    const date1 = new Date("2020-01-01");
+    const date2 = new Date("2020-01-02");
+    expect(calculateDayBetweenDates(date1, date2)).to.equal(1);
+  });
 });
